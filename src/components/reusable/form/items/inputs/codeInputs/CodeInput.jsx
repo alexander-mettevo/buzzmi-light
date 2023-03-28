9af4706e02e54838655602eca87a0e6fe1eb7b79
Alexander Matvienko <01antizyk.it@gmail.com>
2023-03-28 14:00:01 +0300

    add functional for code input

--- src/components/reusable/form/items/inputs/codeInputs/CodeInput.jsx
+++ src/components/reusable/form/items/inputs/codeInputs/CodeInput.jsx
@@ -6,6 +6,13 @@ const CodeInput = ({inactive, success, setCode, index, setValue, error, regex}) 
   const handleInput = (e) => {
     const value = e.target.value;
 
+    if (e.nativeEvent.inputType === 'deleteContentBackward') {
+      if (inputRef.current.previousElementSibling) {
+        inputRef.current.previousElementSibling.focus();
+      }
+      return;
+    }
+
 
     if (regex.test(value)) {
       inputRef.current.value = value;
@@ -15,7 +22,6 @@ const CodeInput = ({inactive, success, setCode, index, setValue, error, regex}) 
         setValue('code', newCode.join(''));
         return newCode;
       });
-
 
       inputRef.current.blur();
 
@@ -33,14 +39,37 @@ const CodeInput = ({inactive, success, setCode, index, setValue, error, regex}) 
     }
   };
 
+
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'ArrowLeft') {
+      if (inputRef.current.previousElementSibling) {
+        inputRef.current.previousElementSibling.focus();
+      }
+    }
+    if (e.key === 'ArrowRight') {
+      if (inputRef.current.nextElementSibling) {
+        inputRef.current.nextElementSibling.focus();
+      }
+    }
+
+    if (e.key === 'Backspace' && !inputRef.current.value) {
+      if (inputRef.current.previousElementSibling) {
+        inputRef.current.previousElementSibling.value = '';
+        inputRef.current.previousElementSibling.focus();
+      }
+    }
+  }
+
   return (
-      <input
-        type="text"
-        className={`input input_code ${inactive ? '_inactive' : ''} ${success ? '_form-success' : ''} ${error ? '_form-error' : ''}`}
-        maxLength={1}
-        ref={inputRef}
-        onInput={handleInput}
-      />
+    <input
+      type="text"
+      className={`input input_code ${inactive ? '_inactive' : ''} ${success ? '_form-success' : ''} ${error ? '_form-error' : ''}`}
+      maxLength={1}
+      ref={inputRef}
+      onInput={handleInput}
+      onKeyDown={handleKeyPress}
+    />
   );
 };
 
